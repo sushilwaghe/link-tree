9a745cf4754393c0b4eb5922953c1767bc04ec13
sushil <sushilwaghe2169@gmail.com>
2022-06-24 22:39:02 +0530

    Add LinkCard to app.js

---
+++ src/components/LinkCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './LinkCard.css';
+
+function LinkCard() {
+  return (
+    <div className='card-container'>
+      
+    </div>
+  )
+}
+
+export default LinkCard
