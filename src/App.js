ac410a417ecc40cd731c6c5d8943906f953dfa2e
sushil <sushilwaghe2169@gmail.com>
2022-06-29 14:23:17 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -2,10 +2,40 @@ import './App.css';
 import LinkCard from './components/LinkCard';
 
 function App() {
+  const data = [
+  {
+    title: 'linkedin',
+    tagline: 'Follow me on linkedin',
+    username: 'sushilwaghe',
+    link:'https://www.linkedin.com/in/sushil-waghe'
+  },
+  {
+    title: 'github',
+    tagline: 'Follow me on github',
+    username: 'sushilwaghe',
+    link: 'https://github.com/sushilwaghe'
+  },
+  {
+    title: 'facebook',
+    tagline: 'Follow me on facebook',
+    username: 'sushilwaghe',
+    link: 'https://www.facebook.com/sushil.waghe.5'
+  },
+  {
+    title: 'instagram',
+    tagline: 'Follow me on instagram',
+    username: 'sushil_waghe',
+    link: 'https://www.instagram.com/sushil_waghe'
+  },
+]
   return (
     <div className="container">
-    <h2 className='app-title'>Link Card</h2>
-     < LinkCard />
+    <h2 className='app-title'>Link-Tree</h2>
+    {
+      data.map((cardinfo)=>{
+        return <LinkCard title={cardinfo.title} tagline={cardinfo.tagline} username={cardinfo.username} link={cardinfo.link}/>
+      })
+    }
     </div>
   );
 }
--- src/components/LinkCard.js
+++ src/components/LinkCard.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import './LinkCard.css';
 
-function LinkCard() {
+import ImgLinkedin from './../img/linkedin.png';
+import ImgGithub from './../img/github.png';
+import ImgFacebook from './../img/facebook..png';
+import ImgInstagram from './../img/instagram..png';
+
+const ImgMap = {
+  "linkedin": ImgLinkedin,
+  "github": ImgGithub,
+  "facebook": ImgFacebook,
+  "instagram": ImgInstagram,
+}
+
+function LinkCard(props) {
   return (
-    <div className='card-container'>
-      
+    <div className={`card-container bg-${props.title}`}>
+    <a href={props.link} className='card-link'>
+      <div className='card-link-handle'>
+        <div>
+          <img src={ImgMap[props.title]} className='card-img'/>
+        </div>
+        <div className='card-title'>
+          <h3 className='card-name'>{props.username}</h3>
+        </div>
+      </div>
+      <div className='card-description'>
+        <p>{props.tagline}</p>
+      </div>
+      </a>
     </div>
   )
 }
